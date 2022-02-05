99194d6e46003bb81cec5f91749339c30d7bda27
AlexandruNeculaes <alexandru.neculaes@gmail.com>
2022-02-05 18:29:32 +0000

    second commit

---
+++ script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+rm -r fol_1
+rm -r fol_2
+rm .gitignore
+rm README.md
+
+mkdir fol_1
+mkdir fol_2
+
+cd fol_1
+touch 1_1.txt
+touch 1_2.txt
+touch 1_3.txt
+
+cd ../fol_2
+touch 2_1.txt
+touch 2_2.txt
+touch 2_3.txt
+
+touch .gitignore
+
+echo 'fol_1/' >> .gitignore
+echo 'fol_2' >> .gitignore
